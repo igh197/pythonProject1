c0e8c67573df63778b14a4a2663807678ffa352c
igh197 <ingyuhan497@gmail.com>
2024-03-27 19:10:00 +0900

    revision1

--- pythonProject1/urls.py
+++ pythonProject1/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for pythonProject1 project.
+from typing import Dict
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.http import HttpRequest
 from django.urls import path
+from ninja import NinjaAPI
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+api = NinjaAPI()
+
+
+@api.get("/add")
+def add(request: HttpRequest, a: int, b: int) -> Dict[str, int]:
+    return {"result": a + b}
+
+
+urlpatterns = [path("admin/", admin.site.urls), path("api/", api.urls)]
